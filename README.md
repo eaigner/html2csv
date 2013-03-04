f8a39389c34ad9dea751701f9e00677039d5a939
Erik Aigner <erik@chocomoko.com>
2013-03-04 23:42:07 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,34 @@
+# Installing
+
+1. Install Xcode from the Mac App Store
+2. Install Homebrew
+
+    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+3. Install libxml2 and libxslt
+
+    brew install libxml2 libxslt
+    brew link libxml2 libxslt --force
+
+4. Install bundler
+
+    sudo gem install bundler
+
+5. Change to the directory parse.rb is in and execute
+
+    bundle install
+    chmod +x parse.rb
+
+6. View the help
+
+    ./parse.rb --help
+
+7. Run!
+
+# Usage
+
+    Usage: parse.rb [options]
+
+    -d, --data DIR                   Directory containing HTML files
+    -o, --output DIR                 Output CSV file name
+    -x, --xpaths FILE                File containing XPaths to match
---
+++ parse.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'optparse'
+require 'nokogiri'
+require 'fastercsv'
+require 'iconv'
+require 'pp'
+
+# Read command line options
+options = {
+  :data_dir => 'data',
+  :output => 'out.csv',
+  :xpath_file => 'Xpaths',
+}
+OptionParser.new do |opts|
+  opts.banner = "Usage: parse.rb [options]"
+  opts.on("-d", "--data DIR", "Directory containing HTML files") do |v|
+    options[:data_dir] = v
+  end
+  opts.on("-o", "--output DIR", "Output CSV file name") do |v|
+    options[:output] = v
+  end
+  opts.on("-x", "--xpaths FILE", "File containing XPaths to match") do |v|
+    options[:xpath_file] = v
+  end
+end.parse!
+
+# Run parser
+class Parser
+  @options = {}
+  def initialize(opts)
+    @options = opts
+
+    # Get xpaths
+    @xpaths = {}
+    File.readlines(opts[:xpath_file]).each do |line|
+      name, xpath = line.match(/^(\w+):\s+(.*)$/i).captures
+      @xpaths[name] = xpath
+    end
+  end
+
+  def parse
+    pp @options
+    pp @xpaths
+    rows = []
+    Dir.foreach(@options[:data_dir]) do |f|
+      unless f.start_with? "."
+        rows.push(process_file(f))
+      end
+    end
+
+    FasterCSV.open(@options[:output], "w", {:col_sep => ";"}) do |csv|
+      csv << @xpaths.keys
+      rows.each { |row|
+        csv << row
+      }
+    end
+  end
+
+  def process_file(f)
+    pp "Processing: #{f}"
+
+    path = File.join(@options[:data_dir], f)
+    doc = Nokogiri::HTML(open(path))
+
+    # Find encoding
+    enc = doc.encoding
+
+    # Extract tags
+    out = []
+    @xpaths.each { |k, v|
+      doc.xpath(v).each { |t|
+        out.push Iconv.conv('US-ASCII//IGNORE', enc, t.to_s)
+      }
+    }
+
+    return out
+  end
+end
+
+Parser.new(options).parse
